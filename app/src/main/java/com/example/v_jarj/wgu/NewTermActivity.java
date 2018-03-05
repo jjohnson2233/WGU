b579999d4aec65d595c51e80060b92f403f81bd7
Jared Johnson <jaredjohnswine@icloud.com>
2018-03-05 15:27:03 -0800

    Created new term page and added functionality to new term button.

---
+++ app/src/main/java/com/example/v_jarj/wgu/NewTermActivity.java
@@ -0,0 +1,13 @@
+package com.example.v_jarj.wgu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewTermActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_term);
+    }
+}
--- app/src/main/java/com/example/v_jarj/wgu/TermsListActivity.java
+++ app/src/main/java/com/example/v_jarj/wgu/TermsListActivity.java
@@ -1,5 +1,6 @@
 package com.example.v_jarj.wgu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,8 +23,7 @@ public class TermsListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(TermsListActivity.this, NewTermActivity.class));
             }
         });
     }
