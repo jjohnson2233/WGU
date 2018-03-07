6d50d6c52f7b9e09defe73972ec1eaeaf375a5d5
Jared Johnson <jaredjohnswine@icloud.com>
2018-03-06 16:20:21 -0800

    Going home for the day

--- app/src/main/java/com/example/v_jarj/wgu/NewTermActivity.java
+++ app/src/main/java/com/example/v_jarj/wgu/NewTermActivity.java
@@ -1,28 +1,19 @@
 package com.example.v_jarj.wgu;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import java.util.Calendar;
 
 public class NewTermActivity extends Activity {
 
-    private TextView startDate = (TextView) findViewById(R.id.startDate);
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_term);
-
-        startDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar calendar = Calendar.getInstance();
-                int year = calendar.get(Calendar.YEAR);
-            }
-        });
     }
 }
