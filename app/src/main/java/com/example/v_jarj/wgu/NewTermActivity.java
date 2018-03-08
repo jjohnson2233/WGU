d4d7983a20a5f309c83973e8c0c3f922ab39dc58
jared <jaredjohnswine@icloud.com>
2018-03-07 20:18:34 -0800

    Fixed the action bar on new term activity.

--- app/src/main/java/com/example/v_jarj/wgu/NewTermActivity.java
+++ app/src/main/java/com/example/v_jarj/wgu/NewTermActivity.java
@@ -1,13 +1,21 @@
 package com.example.v_jarj.wgu;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class NewTermActivity extends Activity {
+public class NewTermActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_term);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
 }
