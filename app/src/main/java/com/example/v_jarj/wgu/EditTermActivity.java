14567ee8dc94f5cdf60dc18afc93cc5c9bedc52a
Jared Johnson <jaredjohnswine@icloud.com>
2018-03-08 15:58:48 -0800

    Adjusted icons to open date pickers on new term and created the UI for edit term.

--- app/src/main/java/com/example/v_jarj/wgu/EditTermActivity.java
+++ app/src/main/java/com/example/v_jarj/wgu/EditTermActivity.java
@@ -1,6 +1,5 @@
 package com.example.v_jarj.wgu;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,22 +7,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class TermInfoActivity extends AppCompatActivity {
+public class EditTermActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_term_info);
+        setContentView(R.layout.activity_edit_term);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("Term Info");
+        toolbar.setTitle("Edit Term");
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(TermInfoActivity.this, EditTermActivity.class);
-                startActivity(intent);
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
