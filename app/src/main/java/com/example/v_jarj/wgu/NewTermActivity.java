60ba0f95da1cfa184777288711de8a4112c4dbed
Jared Johnson <jaredjohnswine@icloud.com>
2018-03-08 10:42:54 -0800

    Worked on the UI for new term activity

--- app/src/main/java/com/example/v_jarj/wgu/NewTermActivity.java
+++ app/src/main/java/com/example/v_jarj/wgu/NewTermActivity.java
@@ -1,5 +1,7 @@
 package com.example.v_jarj.wgu;
 
+import android.app.DialogFragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,8 +16,22 @@ public class NewTermActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_term);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("New Term");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
+    public void openStartDatePicker(View view) {
+    }
+
+    public void openEndDatePicker(View view) {
+    }
 }
