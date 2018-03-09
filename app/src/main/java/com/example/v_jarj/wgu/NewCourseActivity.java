54ee9b1cddfd36c94fd6daa9270b5c208706354f
Jared Johnson <jaredjohnswine@icloud.com>
2018-03-08 16:32:07 -0800

    Gone for the day

--- app/src/main/java/com/example/v_jarj/wgu/NewCourseActivity.java
+++ app/src/main/java/com/example/v_jarj/wgu/NewCourseActivity.java
@@ -1,6 +1,5 @@
 package com.example.v_jarj.wgu;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,25 +7,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class CourseListActivity extends AppCompatActivity {
+public class NewCourseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_course_list);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("Courses");
+        setContentView(R.layout.activity_new_course);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("New Course");
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(CourseListActivity.this, NewCourseActivity.class);
-                startActivity(intent);
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
