1bc7c479c90a1e90e38df4c7dbca03fff75fb3a6
jared <jaredjohnswine@icloud.com>
2018-03-11 13:48:11 -0700

    Got the content provider to work and set up an example method for filling up the terms list.

--- app/src/main/java/com/example/v_jarj/wgu/TermsListActivity.java
+++ app/src/main/java/com/example/v_jarj/wgu/TermsListActivity.java
@@ -1,21 +1,25 @@
 package com.example.v_jarj.wgu;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
-public class TermsListActivity extends AppCompatActivity {
+public class TermsListActivity extends AppCompatActivity
+implements LoaderManager.LoaderCallbacks<Cursor>
+{
+    private CursorAdapter cursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +30,15 @@ public class TermsListActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Terms");
 
-        createTerm("New Term");
-
-        Cursor cursor = getContentResolver().query(DataProvider.TERMS_URI,
-                DBOpenHelper.TERMS_ALL_COLUMNS, null, null, null, null);
         String[] from = {DBOpenHelper.TERM_TITLE};
         int[] to = {android.R.id.text1};
-        CursorAdapter cursorAdapter = new SimpleCursorAdapter(this,
-              android.R.layout.simple_list_item_1, cursor, from, to, 0);
+        cursorAdapter = new SimpleCursorAdapter(this,
+              android.R.layout.simple_list_item_1, null, from, to, 0);
 
         ListView list = findViewById(android.R.id.list);
         list.setAdapter(cursorAdapter);
 
+        getLoaderManager().initLoader(0, null, this);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -59,4 +60,19 @@ public class TermsListActivity extends AppCompatActivity {
                 values);
     }
 
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(this, DataProvider.TERMS_URI,
+                null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        cursorAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        cursorAdapter.swapCursor(null);
+    }
 }
